70cde759e195574dcc6e74528fa8ac06775f9166
DJ Stahlberger <stahlberd8@students.rowan.edu>
2018-09-27 21:58:48 -0400

    All labs for the G2

---
+++ Button Based Delay/MSP430G2553/main.c
@@ -0,0 +1,60 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	BCSCTL3 = LFXT1S_2; //Allows G2 to use internal crystal
+	
+	P1DIR |= BIT0; //Sets direction of P1.0 to output
+
+	P1DIR |= BIT6; //Sets P1.6 to output
+	P1OUT &= ~BIT6;
+
+	P1OUT |= BIT3; //Sets pull-down resistor
+	P1DIR &= ~BIT3; //Sets direction of P1.3 to input
+	P1REN |= BIT3; //Enables the pull up/down resistor
+	P1OUT |= BIT3; //Sets it to pull down
+
+	P1DIR &= ~BIT3; //Sets P1.3 to input
+
+	TACTL = TASSEL_1 | MC_1 | ID_2; //Enables ACLCK, sets up mode, sets internal divider to 4
+	CCTL0 = CCIE; //Interrupt Enable
+	TACCR0 = 200; //sets register 200
+	P1IE |= BIT3; //enables button interrupt
+	P1IES |= BIT3; //sets interrupt to trigger on rising edge
+	P1IFG &= ~BIT3; //clears interrupt flag
+
+    // Enter LPM0 and enable global interrupts
+	_BIS_SR(LPM0_bits + GIE);
+	return 0;
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void Port_1(void){
+    if(P1IES & BIT3){ //if FALLING edge is detected
+        TACTL = TASSEL_1 | MC_2 | ID_2 | TACLR; //Enables ACLCK, sets continuous mode, sets internal divider to 4, CLEARS TIMER
+        P1IES &= ~BIT3; //sets interrupt to falling edge
+    }
+    else{
+        TACTL = TASSEL_1 | MC_1 | ID_2; //Enables ACLCK, sets up mode, sets internal divider to 4
+        P1IES |= BIT3; //sets interrupt to detect falling edge
+        CCR0 = TAR;//sets ccr equal to clck register
+    }
+    P1IFG &= ~BIT3; //Clears interrupt
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0(void){
+    //toggle LED 1.0
+    P1OUT ^= BIT0;
+}
+
+
+
+
+
+
---
+++ TIMER A Blink/G2553/main.c
@@ -0,0 +1,45 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD; //stops watchdog timer
+    BCSCTL3 = LFXT1S_2; //Uses the G2's internal crystal
+    // Pin setup
+    P1SEL &= ~BIT0; // Set P1.0 as a digital input/output
+    P1SEL &= ~BIT6; //Set P1.6 as a digital input/output
+    P1DIR |= BIT0; // Set P1.0 as output
+    P1DIR |= BIT6; // Set p1.6 as output
+
+    // Timer setup
+    TA0CTL = TASSEL_2 | ID_3 | MC_2;
+    // CCR0 setup
+    TA0CCR0 = 0x6000;
+    TA0CCTL0 |= CCIE; //Enables interupt
+
+    //Timer2 Setup
+    TA1CTL = TASSEL_2 | ID_3 | MC_2;
+    //CCR0 setup
+    TA1CCR0 = 0X9000;
+    TA1CCTL0 |= CCIE; //Interrupt enable
+
+    // Enter LPM0 and enable global interrupts
+    _BIS_SR(LPM0_bits + GIE);
+}
+
+// ISR
+#pragma vector=TIMER0_A0_VECTOR;
+__interrupt void Time0_A0_Vector(void) {
+    //LED1.0 is toggled by timer0
+    P1OUT ^= BIT0;
+}
+
+#pragma vector=TIMER1_A0_VECTOR;
+__interrupt void Time1_A1_Vector(void){
+    //LED1.6 is toggled by timer1
+    P1OUT ^= BIT6;
+
+
+}
